0525f9b63001f9a0323ad23f284a970c363eb2e0
taejunUM <utj0411@naver.com>
2025-01-03 15:27:45 +0900

    StartProject

--- src/main/java/com/example/schedule/filter/LoginFilter.java
+++ src/main/java/com/example/schedule/filter/LoginFilter.java
@@ -1,45 +1,45 @@
-package com.example.schedule.filter;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.PatternMatchUtils;
-
-import java.io.IOException;
-
-@Slf4j
-public class LoginFilter implements Filter {
-
-    private static final String[] WHITE_LIST = {"/", "/user/signup", "/login", "/logout"};
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String requestURI = httpRequest.getRequestURI();
-
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        log.info("Excute login filter logic");
-
-        if (!isWhiteList(requestURI)) {
-
-            HttpSession session = httpRequest.getSession(false);
-
-            if (session == null || session.getAttribute("sessionKey") == null) {
-                throw new RuntimeException("Please Login");
-            }
-
-            log.info("Login was successful.");
-        }
-
-        chain.doFilter(request, response);
-    }
-
-    private boolean isWhiteList(String requestURI) {
-        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
-    }
-}
+//package com.example.schedule.filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.util.PatternMatchUtils;
+//
+//import java.io.IOException;
+//
+//@Slf4j
+//public class LoginFilter implements Filter {
+//
+//    private static final String[] WHITE_LIST = {"/", "/user/signup", "/login", "/logout"};
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        String requestURI = httpRequest.getRequestURI();
+//
+//        HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//        log.info("Excute login filter logic");
+//
+//        if (!isWhiteList(requestURI)) {
+//
+//            HttpSession session = httpRequest.getSession(false);
+//
+//            if (session == null || session.getAttribute("sessionKey") == null) {
+//                throw new RuntimeException("Please Login");
+//            }
+//
+//            log.info("Login was successful.");
+//        }
+//
+//        chain.doFilter(request, response);
+//    }
+//
+//    private boolean isWhiteList(String requestURI) {
+//        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+//    }
+//}
